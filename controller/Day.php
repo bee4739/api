f3263ca58670d43afa625a03cd946d5e8b527c2c
bee4739 <beenoii.rn@gmail.com>
2021-04-24 01:27:17 +0700

    show data calendar

--- controller/Day.php
+++ controller/Day.php
@@ -13,7 +13,29 @@ class Day
   {
     $db = new \Tools\Database();
     $rawData = json_decode(file_get_contents('php://input'), true);
-    $query = $db->query("SELECT `tb_schedule`.`Schedule_ID`, `tb_schedule`.`Day`, `tb_class`.`Class_ID`, `tb_year`.`Year`, `tb_year`.`Term`, `tb_year`.`Start_SchYear`, `tb_year`.`End_SchYear` FROM `tb_schedule` LEFT JOIN `tb_class` ON `tb_schedule`.`Class_ID` = `tb_class`.`Class_ID` LEFT JOIN `tb_year` ON `tb_class`.`Year_ID` = `tb_year`.`Year_ID`
+    $query = $db->query("SELECT 
+    `tb_schedule`.`Schedule_ID`, 
+    `tb_schedule`.`Day`,
+    `tb_schedule`.`Start_Time`,
+    `tb_schedule`.`End_Time`,
+    `tb_schedule`.`Subject_Type`,
+    `tb_class`.`Class_ID`, 
+    `tb_class`.`Group_Study`,
+    `tb_year`.`Year`, 
+    `tb_year`.`Term`, 
+    `tb_year`.`Start_SchYear`, 
+    `tb_year`.`End_SchYear`,
+    `tb_subject`.`Subject_ID`,
+    `tb_subject`.`Subject_NameTH`,
+    `tb_subject`.`Subject_Theory`,
+    `tb_subject`.`Subject_Practice`
+  FROM `tb_schedule` 
+  LEFT JOIN `tb_class` 
+  ON `tb_schedule`.`Class_ID` = `tb_class`.`Class_ID` 
+  LEFT JOIN `tb_year` 
+  ON `tb_class`.`Year_ID` = `tb_year`.`Year_ID`
+  LEFT JOIN `tb_subject` 
+  ON `tb_class`.`Subject_PK` = `tb_subject`.`Subject_PK`
          ");
     // $day = array();
     // $startDate = '2021-04-01';
